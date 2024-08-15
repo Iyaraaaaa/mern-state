b803add75c2954dcea79bd4865abff89f66d6f4f
Sithmi Iyara <you@example.com>
2024-08-15 10:57:37 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignUp from './Pages/SignUp';
+import SignOut from './Pages/SignOut';
+import Profile from './Pages/Profile';
+import About from './Pages/About'; // Import the About component
 
 export default function App() {
   return (
-    <dh1 className = "text-red-500">
-      App
-    </dh1>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signout' element={<SignOut />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
